35f98b0a6c9b2da2e96975c4a25a29982bfd5b5b
Pradeep Dilruksha <108401187+Lavonetech@users.noreply.github.com>
2024-01-24 00:23:38 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -42,7 +42,7 @@ app.use('/transaction',transactionRoutes);
 
 
 
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  // app.use(express.static(path.join(__dirname, '../client/build')));
 
   // app.get('*', (req, res) =>
   //   res.sendFile(
