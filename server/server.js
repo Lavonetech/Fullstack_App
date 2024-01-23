3e560265a5a4a849f0c461a88cfa3a29e932cd7f
Pradeep Dilruksha <108401187+Lavonetech@users.noreply.github.com>
2024-01-23 23:26:25 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -45,11 +45,11 @@ app.use('/transaction',transactionRoutes);
 
   app.use(express.static(path.join(__dirname, '../client/build')));
 
-  app.get('*', (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, '../', 'client', 'build', 'index.html')
-    )
-  );
+  // app.get('*', (req, res) =>
+  //   res.sendFile(
+  //     path.resolve(__dirname, '../', 'client', 'build', 'index.html')
+  //   )
+  // );
 
 //mongoose setup
 
